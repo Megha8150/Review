3e190e606f83361a73dabf92c4c1da0d3277bed1
Megha8150 <meghananayak436@gmail.com>
2022-06-23 18:28:15 +0530

    added Parttime

--- Employeewage.sh
+++ Employeewage.sh
@@ -4,16 +4,22 @@ echo "Welcome to Employee Wage Computation Program"
 
 Wage_per_hour=20
 Full_day_hour=8
+Half_day_hour=4
 Daily_wage=0
 
-a=$((RANDOM%2))
+a=$((RANDOM%3))
 
 if [ $a -eq 0 ]
 then
-	echo -e "Employee is present\n"
+	echo  "Employee is present"
 	Daily_wage=$(($Wage_per_hour*$Full_day_hour))
 	echo "Employee Daily wage is " $Daily_wage
+elif [ $a -eq 1 ]
+then
+	echo  "Employee working partime"
+	Daily_wage=$(($Wage_per_hour*$Half_day_hour))
+        echo "Employee Daily wage is " $Daily_wage
 else
 	echo "Employee is absent"
-	echo "Employee Daily wage is " $Daily_wage
+
 fi
